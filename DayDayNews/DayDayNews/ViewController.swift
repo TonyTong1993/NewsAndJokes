b486ab54e1549a46b353ee87711f5ed883b62109
马卿 <admin@maiqngdemac-mini.local>
2016-09-28 09:16:02 +0800

    DayDayNews:项目初始化

---
+++ DayDayNews/DayDayNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DayDayNews
+//
+//  Created by 马卿 on 16/9/28.
+//  Copyright © 2016年 com.yihe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
