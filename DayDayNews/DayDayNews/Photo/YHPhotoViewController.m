28ceaa058b4a5c18bd3d8899fe2f5ef01ec3c5c9
马卿 <admin@maiqngdemac-mini.local>
2016-09-28 17:29:04 +0800

    图片：添加下拉刷新控件，模拟数据延迟加载

--- DayDayNews/DayDayNews/Photo/YHPhotoViewController.m
+++ DayDayNews/DayDayNews/Photo/YHPhotoViewController.m
@@ -9,35 +9,57 @@
 #import "YHPhotoViewController.h"
 
 @interface YHPhotoViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
-
+@property (nonatomic,strong) UICollectionView *collectionView;
+@property (nonatomic,strong) UIRefreshControl *refreshControl;
 @end
 #define CellID @"cell"
 @implementation YHPhotoViewController
-
+#pragma mark ---lazy load
+-(UICollectionView *)collectionView{
+    if (!_collectionView) {
+          CGRect rect = [UIScreen mainScreen].bounds;
+        _collectionView = [[UICollectionView alloc]initWithFrame:rect collectionViewLayout:[[UICollectionViewFlowLayout alloc]init]];
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        _collectionView.backgroundColor = [UIColor blackColor];
+        //注册class
+        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:CellID];
+        //设置collectionView 边距
+        UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout*)_collectionView.collectionViewLayout;
+        //items 之间上下间距
+        layout.minimumLineSpacing = 5.0f;
+        //设置item的尺寸
+        layout.itemSize = CGSizeMake(100, 100);
+        //指明滑动方向
+        layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+    }
+    return _collectionView;
+}
+-(UIRefreshControl *)refreshControl{
+    if (!_refreshControl) {
+        _refreshControl = [[UIRefreshControl alloc] init];
+        [_refreshControl addTarget:self action:@selector(loadData) forControlEvents:UIControlEventValueChanged];
+    }
+    return _refreshControl;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     [self setupView];
+}
+#pragma mark---获取数据
+-(void)loadData{
+    //模拟延迟加载数据
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        //关闭刷新控件
+        [_refreshControl endRefreshing];
+        //刷新数据
+        [_collectionView reloadData];
+    });
 }
 #pragma mark---设置界面
 -(void)setupView{
-    CGRect rect = [UIScreen mainScreen].bounds;
-    //指明流水布局
-   UICollectionView *collectionView = [[UICollectionView alloc]initWithFrame:rect collectionViewLayout:[[UICollectionViewFlowLayout alloc]init]];
-    collectionView.delegate = self;
-    collectionView.dataSource = self;
-    collectionView.backgroundColor = [UIColor blackColor];
-    //注册class
-    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:CellID];
-    [self.view addSubview:collectionView];
-    //设置collectionView 边距
-   UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout*)collectionView.collectionViewLayout;
-   layout.minimumLineSpacing = 5.0f;
-    //设置item的尺寸
-   layout.itemSize = CGSizeMake(100, 100);
-    //指明滑动方向
-   layout.scrollDirection = UICollectionViewScrollDirectionVertical;
-    
+   [self.view addSubview:self.collectionView];
+   [_collectionView addSubview:self.refreshControl];
 }
 #pragma mark---UICollectionViewDataSource
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
