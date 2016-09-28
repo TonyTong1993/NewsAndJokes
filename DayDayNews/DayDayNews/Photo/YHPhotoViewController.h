6552cd89ea25b69bde9896821ece566b1c534a35
马卿 <admin@maiqngdemac-mini.local>
2016-09-28 16:39:29 +0800

    图片：图片主页视图构建

---
+++ DayDayNews/DayDayNews/Photo/YHPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  YHPhotoViewController.h
+//  DayDayNews
+//
+//  Created by 马卿 on 16/9/28.
+//  Copyright © 2016年 com.yihe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YHPhotoViewController : UIViewController
+
+@end
